2312bbc73d8d0ba034c8db6de2f3556e6c7ce76a
yuuma <tsuji@syspla.net>
2018-06-26 14:24:31 +0900

    init

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -u
+
+# 実行場所のディレクトリを取得
+THIS_DIR=$(cd $(dirname $0); pwd)
+
+cd $THIS_DIR
+
+echo "start setup..."
+for f in .??*; do
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".gitconfig.local.template" ] && continue
+    [ "$f" = ".require_oh-my-zsh" ] && continue
+    [ "$f" = ".gitmodules" ] && continue
+
+    ln -snfv ~/dotfiles/"$f" ~/
+done
